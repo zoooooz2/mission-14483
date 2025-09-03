ef5b0bfad2a838ec0d09872caacdbdc38d2f7c36
zoooooz2 <92962527+zoooooz2@users.noreply.github.com>
2025-09-03 09:47:17 +0900

    feat(Calc): t2 테스트 통과

--- tddcalculator/src/test/java/com/tddcalculator/CalcTest.java
+++ tddcalculator/src/test/java/com/tddcalculator/CalcTest.java
@@ -7,10 +7,16 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
 class CalcTest {
+
     @Test
     @DisplayName("1 + 1 == 2")
     void t1() {
         assertThat(Calc.run("1 + 1")).isEqualTo(2);
     }
 
+    @Test
+    @DisplayName("2 + 1 == 3")
+    void t2() {
+        assertThat(Calc.run("2 + 1")).isEqualTo(3);
+    }
 }
