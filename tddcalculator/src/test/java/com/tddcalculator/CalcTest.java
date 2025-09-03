5d18acbbaa7f2668aeacb25876a1bffc2b2d813b
zoooooz2 <92962527+zoooooz2@users.noreply.github.com>
2025-09-03 11:13:07 +0900

    feat(Calc): t9 테스트 통과

--- tddcalculator/src/test/java/com/tddcalculator/CalcTest.java
+++ tddcalculator/src/test/java/com/tddcalculator/CalcTest.java
@@ -55,4 +55,10 @@ class CalcTest {
     void t8() {
         assertThat(Calc.run("10 + 20 + 30")).isEqualTo(60);
     }
+
+    @Test
+    @DisplayName("10 - 20 + 30 == 20")
+    void t9() {
+        assertThat(Calc.run("10 - 20 + 30")).isEqualTo(20);
+    }
 }
